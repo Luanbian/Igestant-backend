97e625186f1ca26351f57869452df522219ef2c9
Luan Bianchini <luanbianchine1@gmail.com>
2023-12-15 13:43:54 -0300

    feat: implement middleware in context

--- src/infra/graphql/factories/index.ts
+++ src/infra/graphql/factories/index.ts
@@ -1,5 +1,7 @@
+import { type Context } from '../../../main'
+
 export type ResolverFunction<Args, Result> = (args: Args, ctx: any) => Result
 
 export function makeResolver<Args = unknown, Result = any> (func: ResolverFunction<Args, Result>) {
-  return (_: unknown, args: Args, ctx: any) => func(args, ctx)
+  return (_: unknown, args: Args, ctx: Context) => func(args, ctx)
 }
--- src/main/index.ts
+++ src/main/index.ts
@@ -2,11 +2,19 @@ import 'reflect-metadata'
 import { startStandaloneServer } from '@apollo/server/standalone'
 import { makeServer } from '../infra/graphql/server'
 import path from 'path'
+import { middleware } from './middleware'
+
+export interface Context {
+  token?: string
+}
 
 export async function bootstrap (): Promise<void> {
   const schemaPath = path.resolve(__dirname, '../infra/graphql/schema/*.gql')
   const server = makeServer(schemaPath)
-  const { url } = await startStandaloneServer(server, {
+  const { url } = await startStandaloneServer<Context>(server, {
+    context: async ({ req }) => ({
+      token: await middleware(req.headers.authorization)
+    }),
     listen: {
       port: 4000
     }
