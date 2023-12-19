719e8e88b24713add30a093850c971ddcb3d4787
luan <luanbianchine1@gmail.com>
2023-12-19 16:49:50 -0300

    refactor: invert logic of token to be safer

--- src/infra/graphql/contexts/index.ts
+++ src/infra/graphql/contexts/index.ts
@@ -5,22 +5,16 @@ export interface Context {
 }
 
 const context = async ({ req }): Promise<Context> => {
-  const { body, headers } = req
-  const { operationName } = body
+  const allowedOperations = ['readCard', 'IntrospectionQuery', 'LoginUser']
 
-  if (isCardOperation(operationName as string)) {
-    const authToken: string = headers.authorization ?? ''
+  if (!allowedOperations.includes(req.body.operationName as string)) {
+    const authToken: string = req.headers.authorization ?? ''
     return {
       token: await middleware(authToken)
     }
   }
 
   return {}
-}
-
-const isCardOperation = (operationName: string): boolean => {
-  const cardOperations = ['createCard', 'updateCard', 'deleteCard', 'Mutation']
-  return cardOperations.includes(operationName)
 }
 
 export default context
